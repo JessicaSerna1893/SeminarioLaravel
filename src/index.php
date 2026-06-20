cc704630066cb2a53bc6c709c98a7a2340d6ade0
JessicaSerna1893 <jessica-serna@cotecnova.edu.co>
2026-06-20 11:30:04 -0500

    Actualización index.php e imagen

--- src/index.php
+++ src/index.php
@@ -1,3 +1,22 @@
 <?php
-phpinfo();
+
+// 1. Variables y Strings
+$nombre = "Jessica Serna";
+$edad = 33;
+$ciudad = "Cartago";
+$carrera = "Ingenieria de Sistemas";
+
+// 2. Nuevas variables
+$semestre = 9;
+$materiaFavorita = "Matematicas";
+// 3. Mostrar en HTML
+echo "<h1>Mi presentación</h1>";
+echo "<p>Hola, soy <strong>$nombre</strong>, tengo $edad años, vivo en $ciudad y estudio $carrera.</p>";
+
+// 4. Mostrar semestre y materia favorita
+echo "<p>Estoy en $semestre semestre y mi materia favorita es $materiaFavorita.</p>";
+
+// 5. Diferencia entre comillas simples y dobles
+echo '<p>Con comillas simples: $nombre no se interpreta</p>';
+
 ?>
